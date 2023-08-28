d9bb4582786091ffaf2a2ac006ecfc0182e474bf
Dulce Jaramillo <dulcejaramillo@dulces-MacBook-Pro.local>
2023-08-27 23:46:05 -0700

    added stuff

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-// In this assignment, you will be creating a simple webpage using HTML, CSS, and JavaScript. The webpage should have the following components:
+let rainbow = document.getElementById("rainbow")
+let message1 = document.getElementById("message1")
+let message2 = document.getElementById("message2")
+let message = document.getElementsByTagName("p")
 
-// A heading that displays the title of the webpage.
-// An image that is centered on the webpage.
-// A paragraph of text that is centered below the image.
-// A button that changes the text of the paragraph when clicked.
+rainbow.addEventListener("click", function(){
+   message1.textContent=  "They love going to sporting events, concerts and going on Road Trips!"
+   
+} );
+
+
+
+
+
+
+
 // A text input field that updates the paragraph text as the user types.
 // You should use CSS to style the webpage, including the font and color of the text, the size and position of the image, and the background color of the webpage. You should use JavaScript to handle the button click event and input field change event, and update the paragraph text accordingly.
 
 
-console.log ("hello")
