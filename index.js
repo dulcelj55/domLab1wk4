9bd37536831d28ea911edee8dcd8f7cf04d525c9
Dulce Jaramillo <dulcejaramillo@dulces-MacBook-Pro.local>
2023-08-27 22:04:28 -0700

    start of project

---
+++ index.js
@@ -0,0 +1,11 @@
+// In this assignment, you will be creating a simple webpage using HTML, CSS, and JavaScript. The webpage should have the following components:
+
+// A heading that displays the title of the webpage.
+// An image that is centered on the webpage.
+// A paragraph of text that is centered below the image.
+// A button that changes the text of the paragraph when clicked.
+// A text input field that updates the paragraph text as the user types.
+// You should use CSS to style the webpage, including the font and color of the text, the size and position of the image, and the background color of the webpage. You should use JavaScript to handle the button click event and input field change event, and update the paragraph text accordingly.
+
+
+console.log ("hello")
